07a4908c3320c4339cf5a7b878b9d965594226e3
BilykIgor <igoriw80@gmail.com>
2024-12-17 22:20:57 +0000

    add more options for select html element in filter component

--- src/components/Filter/ExpensesFilter.js
+++ src/components/Filter/ExpensesFilter.js
@@ -11,6 +11,8 @@ const ExpensesFilter = (props) => {
       <div className="expenses-filter__control">
         <label>Filter by year</label>
         <select onChange={handleSelect} value={props.year}>
+          <option value="2025">2025</option>
+          <option value="2024">2024</option>
           <option value="2023">2023</option>
           <option value="2022">2022</option>
           <option value="2021">2021</option>
